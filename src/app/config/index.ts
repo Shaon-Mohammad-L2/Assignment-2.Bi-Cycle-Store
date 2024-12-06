d1670da9c632f18feda48f636f9289daa2a6a4ef
Shaon-Mohammad-L2 <shaon.mohammad.l2@gmail.com>
2024-12-06 09:50:56 +0600

    vercel deploy isshue

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,6 +4,6 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-   port: process.env.PORT,
+   port: process.env.PORT || 3000,
    db_url: process.env.DATA_BASE_URL,
 }
